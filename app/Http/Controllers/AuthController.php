cf56b82141027ec398eba4ecae12f0945a354f1a
Alwisteins <alwi.wahyu.w@gmail.com>
2025-09-15 18:21:45 +0700

    tambah dikit

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -31,6 +31,8 @@ class AuthController extends Controller
     }
 
     public function logout(Request $request) {
+        Auth::logout();
+
         $request->session()->invalidate();
         $request->session()->regenerateToken();
 
